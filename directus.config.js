b4122b36d558e2d7aa511a8f0ebd2a964577de91
Matt Hanes <hanes.matt@gmail.com>
2026-06-30 20:08:12 -0400

    Update Directus to v12

--- directus.config.js
+++ directus.config.js
@@ -1,32 +1,35 @@
+/**
+ * @param {Record<string, string>} env - Environment variables
+ */
 
 module.exports = function (env) {
-    return {
-        // User inputs from railway starter button
-        ADMIN_EMAIL: env.ADMIN_EMAIL,
-        ADMIN_PASSWORD: env.ADMIN_PASSWORD,
-        KEY: env.KEY,
-        SECRET: env.SECRET,
+  return {
+    // User inputs from railway starter button
+    ADMIN_EMAIL: env.ADMIN_EMAIL,
+    ADMIN_PASSWORD: env.ADMIN_PASSWORD,
+    KEY: env.KEY,
+    SECRET: env.SECRET,
 
-        // Reference: https://docs.railway.app/deploy/exposing-your-app
-        PORT: env.PORT,
+    // Reference: https://docs.railway.app/deploy/exposing-your-app
+    PORT: env.PORT,
 
-        // Reference: https://docs.railway.app/develop/variables#railway-provided-variables
-        PUBLIC_URL: `https://srblog.srblife.com`,
+    // Reference: https://docs.railway.app/develop/variables#railway-provided-variables
+    PUBLIC_URL: `https://srblog.srblife.com`,
 
-        // Database variables from Railway PostgreSQL Plugin
-        // Reference: https://docs.railway.app/plugins/postgresql
-        DB_CLIENT: 'pg',
-        DB_HOST: env.PGHOST,
-        DB_PORT: env.PGPORT,
-        DB_DATABASE: env.PGDATABASE,
-        DB_USER: env.PGUSER,
-        DB_PASSWORD: env.PGPASSWORD,
-        STORAGE_LOCATIONS: "s3",
-        STORAGE_S3_DRIVER: "s3",
-        STORAGE_S3_KEY: env.STORAGE_S3_KEY,
-        STORAGE_S3_SECRET: env.STORAGE_S3_SECRET,
-        STORAGE_S3_REGION: env.STORAGE_S3_REGION,
-        STORAGE_S3_BUCKET:  env.STORAGE_S3_BUCKET,
-        STORAGE_S3_ENDPOINT: env.STORAGE_S3_ENDPOINT,
-    };
+    // Database variables from Railway PostgreSQL Plugin
+    // Reference: https://docs.railway.app/plugins/postgresql
+    DB_CLIENT: 'pg',
+    DB_HOST: env.PGHOST,
+    DB_PORT: env.PGPORT,
+    DB_DATABASE: env.PGDATABASE,
+    DB_USER: env.PGUSER,
+    DB_PASSWORD: env.PGPASSWORD,
+    STORAGE_LOCATIONS: 's3',
+    STORAGE_S3_DRIVER: 's3',
+    STORAGE_S3_KEY: env.STORAGE_S3_KEY,
+    STORAGE_S3_SECRET: env.STORAGE_S3_SECRET,
+    STORAGE_S3_REGION: env.STORAGE_S3_REGION,
+    STORAGE_S3_BUCKET: env.STORAGE_S3_BUCKET,
+    STORAGE_S3_ENDPOINT: env.STORAGE_S3_ENDPOINT,
+  };
 };
